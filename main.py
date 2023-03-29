d4e5a7cc3c642b44a1b8617000abfdcfa2cec1f2
junior <jhgalvanmintic@gmail.com>
2023-03-28 19:55:51 -0500

    solucion de cors y se crea dockerfile

--- main.py
+++ main.py
@@ -2,70 +2,65 @@ from flask import Flask
 from flask import jsonify
 from flask import request
 from flask_cors import CORS
-# ir a setting , interprete y instalasr flask_jwt_extended
+import json
+from waitress import serve
+import  datetime
+import requests
+import re
+
+
+
+app = Flask(__name__)
+# control de cors info en la documentacion  flask cors
+cors = CORS(app)
+
 from flask_jwt_extended import create_access_token, verify_jwt_in_request
 from flask_jwt_extended import get_jwt_identity
 from flask_jwt_extended import jwt_required
 from flask_jwt_extended import JWTManager
-from waitress import serve
-
-import json
-import datetime
-import requests
-import re
-
-app = Flask(__name__)
-# control de cors info en la documentacion  flask cors
-cors = CORS(app, resources={r"*": {"origins": "*"}})
-# mi contraseña
-app.config["JWT_SECRET_KEY"] = "super-secret"  # Cambiar por el que se conveniente
+app.config["JWT_SECRET_KEY"]="super-secret" #Cambiar por el que sea conveniente
 jwt = JWTManager(app)
 
-def loadFileConfig():
-    with open('config.json') as f:
-        data = json.load(f)
-    return data
-dataConfig = loadFileConfig()
+@app.route("/login", methods=['POST'])
+def validarUsuario():
+    url = dataConfig['url-backend-registraduriasecurity'] + "/usuario/validar-usuario"
+    headers = {"Content-Type": "application/json"}
+    bodyRequest = request.get_json()
+    response = requests.post(url, json=bodyRequest, headers=headers)
+    if (response.status_code == 200):
+        print("el usuario se valido correctamente")
+        infoUsuario = response.json()
+        tiempoToken = datetime.timedelta(seconds=60 * 60)
+        newToken = create_access_token(identity=infoUsuario, expires_delta=tiempoToken)
+        return {"token": newToken}
+    else:
+        print("error en validacion usuario")
+        return {"mensaje": "usuario y contraseña errados"}, 401
 
 
 
-# para que sea la primera en ejecutarce
+
+
+# para que sea la primera en ejecutarce @app.before_request
 @app.before_request
-
 def middleware():
     print("Entro al middleware!!")
     urlCliente = request.path
     metodoCliente = request.method
-
+    urlCliente = transformarUrl(urlCliente)
     if (urlCliente == "/login"):
         print("la url es /login, no valido")
         pass
-    else:
+    elif verify_jwt_in_request():
         print("aqui se valida token")
-
-        verify_jwt_in_request()
-        # para validar los rol
         infoToken = get_jwt_identity()
-        idRol = infoToken["rol"]["_id"]
-
-        #  cambia numeros de la url por ?
-        urlCliente = transformarUrl(urlCliente)
-
-        urlValidarPermiso = dataConfig['url-backend-registraduriasecurity'] + "/permiso-rol/validar-permiso/rol/" + idRol
-        headers = {"Content-Type": "application/json"}
-        print(urlCliente)
-        bodyRequest = {
-            "url": urlCliente,
-            "metodo": metodoCliente
-        }
-        responseValidarPermiso = requests.get(urlValidarPermiso, json=bodyRequest, headers=headers)
-        print("status code del servicio validar permiso", responseValidarPermiso)
-
-        if (responseValidarPermiso.status_code == 200):
-            print("el cliente si tiene permisos")
-            pass
+        if infoToken["rol"] is not None:
+            tienePersmiso = validarPermiso(urlCliente, metodoCliente, infoToken["rol"]["_id"])
+            if not tienePersmiso:
+                return jsonify({"message": "Permission denied"}), 401
         else:
-            return {"mensake": "permiso denegado}"}, 401
+            return jsonify({"message": "Permission denied"}), 401
+
 
 
 def transformarUrl(urlCliente):
@@ -75,27 +70,24 @@ def transformarUrl(urlCliente):
             urlCliente = urlCliente.replace(palabra, "?")
     return urlCliente
 
+def validarPermiso(urlCliente,metodoCliente,idRol):
+    urlValidarPermiso = dataConfig['url-backend-registraduriasecurity'] + "/permiso-rol/validar-permiso/rol/" + idRol
+    tienePermiso=False
+    headers = {"Content-Type": "application/json; charset=utf-8"}
+    bodyRequest = {
+        "url": urlCliente,
+        "metodo": metodoCliente
+    }
+    response = requests.get(urlValidarPermiso,json=bodyRequest, headers=headers)
+    try:
+        data=response.json()
+        if("_id" in data):
+            tienePermiso=True
+    except:
+        pass
+    return tienePermiso
 
-@app.route("/login", methods=['POST'])
-def validarUsuario():
-    url = dataConfig['url-backend-registraduriasecurity'] + "/usuario/validar-usuario"
-
-    headers = {"Content-Type": "application/json"}
-    bodyRequest = request.get_json()
-    response = requests.post(url, json=bodyRequest, headers=headers)
-    print(response.status_code)
-    if (response.status_code == 200):
-        print("el usuario se valido correctamente")
-        infoUsuario = response.json()
-
-        tiempoToken = datetime.timedelta(seconds=60 * 60)
-        newToken = create_access_token(identity=infoUsuario, expires_delta=tiempoToken)
-
-        return {"token": newToken}
-    else:
-        print("error en validacion usuario")
-        return {"mensaje": "usuario y contraseña errados"}, 401
-
+###############################################################################
 
 @app.route("/usuario", methods=['GET'])
 def listarUsuarios():
@@ -478,10 +470,13 @@ def eliminarMesa(idMesa):
 
 # fin rutas mesa
 # ==================================================================
-
-
+def loadFileConfig():
+    with open('config.json') as f:
+        data = json.load(f)
+    return data
 
 
 if __name__ == '__main__':
+    dataConfig = loadFileConfig()
     print("Server running : " + "http://" + dataConfig["url-backend"] + ":" + str(dataConfig["port"]))
     serve(app, host=dataConfig["url-backend"], port=dataConfig["port"])
